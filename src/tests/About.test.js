8aa2cdc94b09c8698b266b23753889f3e1a4408b
Felipe Seabra <feliperoce@gmail.com>
2022-09-19 15:42:06 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,32 @@
-test('', () => {
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
 
+describe('Teste se a página contém as informações sobre a Pokédex;', () => {
+  it('Se contém o Título About Pokédex', () => {
+    renderWithRouter(<About />);
+    const title = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Se contém texto de descrição 1', () => {
+    renderWithRouter(<About />);
+    const description = screen.getByText('This application', { exact: false });
+    expect(description).toBeInTheDocument();
+  });
+
+  it('Se contém texto de descrição 2', () => {
+    renderWithRouter(<About />);
+    const description = screen.getByText('One can filter', { exact: false });
+    expect(description).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const link = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', link);
+    expect(img).toHaveAttribute('alt', 'Pokédex');
+  });
 });
